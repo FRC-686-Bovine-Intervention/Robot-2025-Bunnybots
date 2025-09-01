bc787aba222f29b51260dc452f4db3330f8b0187
Michael Sims <michael@the-sims-family.net>
2025-09-01 14:20:40 -0400

    robot and robotcontainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,149 @@
 
 package frc.robot;
 
+import java.util.Arrays;
+import java.util.Set;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.event.EventLoop;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+import frc.robot.constants.RobotConstants;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.DriveConstants;
+import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIOPigeon2;
+import frc.robot.subsystems.drive.ModuleIO;
+import frc.robot.subsystems.drive.ModuleIOFalcon550;
+import frc.robot.subsystems.drive.ModuleIOSim;
+import frc.robot.subsystems.drive.OdometryTimestampIO;
+import frc.robot.subsystems.drive.OdometryTimestampIO.OdometryTimestampIOOdometryThread;
+import frc.robot.subsystems.drive.OdometryTimestampIO.OdometryTimestampIOSim;
+import frc.robot.subsystems.drive.commands.WheelRadiusCalibration;
+import frc.util.controllers.XboxController;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
-  }
+    // Subsystems
+    public final Drive drive;
+    
+    // Vision
 
-  private void configureBindings() {}
+    // Event Loops
+    public final EventLoop automationsLoop = new EventLoop();
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+    // Controllers
+    private final XboxController driveController = new XboxController(0);
+    @SuppressWarnings("unused")
+    private final CommandJoystick simJoystick = new CommandJoystick(5);
+
+    @SuppressWarnings("resource")
+    public RobotContainer() {
+        System.out.println("[Init RobotContainer] Creating " + RobotType.getMode().name() + " " + RobotType.getRobot().name());
+
+        switch (RobotType.getMode()) {
+            case REAL -> {
+                this.drive = new Drive(
+                    new OdometryTimestampIOOdometryThread(),
+                    new GyroIOPigeon2(),
+                    Arrays.stream(DriveConstants.moduleConstants)
+                        .map(ModuleIOFalcon550::new)
+                        .toArray(ModuleIO[]::new)
+                );
+            }
+            case SIM -> {
+                this.drive = new Drive(
+                    new OdometryTimestampIOSim(),
+                    new GyroIO() {},
+                    Arrays.stream(DriveConstants.moduleConstants)
+                        .map(ModuleIOSim::new)
+                        .toArray(ModuleIO[]::new)
+                );
+            }
+            default -> {
+                this.drive = new Drive(
+                    new OdometryTimestampIO() {},
+                    new GyroIO() {},
+                    new ModuleIO(){},
+                    new ModuleIO(){},
+                    new ModuleIO(){},
+                    new ModuleIO(){}
+                );
+            }
+        }
+
+        System.out.println("[Init RobotContainer] Configuring Commands");
+
+        System.out.println("[Init RobotContainer] Configuring Notifications");
+
+        System.out.println("[Init RobotContainer] Configuring Autonomous Modes");
+
+        System.out.println("[Init RobotContainer] Configuring System Check");
+        SmartDashboard.putData("System Check/Drive/Spin", 
+            new Command() {
+                private final Drive.Rotational rotationalSubsystem = drive.rotationalSubsystem;
+                private final Timer timer = new Timer();
+                {
+                    addRequirements(this.rotationalSubsystem);
+                    setName("TEST Spin");
+                }
+                public void initialize() {
+                    this.timer.restart();
+                }
+                public void execute() {
+                    this.rotationalSubsystem.driveVelocity(Math.sin(this.timer.get()) * 3);
+                }
+                public void end(boolean interrupted) {
+                    this.timer.stop();
+                    this.rotationalSubsystem.stop();
+                }
+            }
+        );
+        SmartDashboard.putData("System Check/Drive/Circle", 
+            new Command() {
+                private final Drive.Translational translationSubsystem = drive.translationSubsystem;
+                private final Timer timer = new Timer();
+                {
+                    addRequirements(this.translationSubsystem);
+                    setName("TEST Circle");
+                }
+                public void initialize() {
+                    this.timer.restart();
+                }
+                public void execute() {
+                    this.translationSubsystem.driveVelocity(
+                        new ChassisSpeeds(
+                            Math.cos(this.timer.get()) * 0.01,
+                            Math.sin(this.timer.get()) * 0.01,
+                            0
+                        )
+                    );
+                }
+                public void end(boolean interrupted) {
+                    this.timer.stop();
+                    this.translationSubsystem.stop();
+                }
+            }
+        );
+        
+        SmartDashboard.putData("Wheel Calibration", Commands.defer(
+            () -> 
+                new WheelRadiusCalibration(
+                    drive,
+                    WheelRadiusCalibration.VOLTAGE_RAMP_RATE.get(),
+                    WheelRadiusCalibration.MAX_VOLTAGE.get()
+                )
+                .withName("Wheel Calibration"),
+                Set.of(drive.translationSubsystem, drive.rotationalSubsystem)
+            )
+        );
+
+        if (RobotConstants.tuningMode) {
+            new Alert("Tuning mode active", AlertType.kInfo).set(true);
+        }
+    }
 }
